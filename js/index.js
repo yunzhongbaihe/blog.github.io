5696bf81c069c700b7b37f167b6791b0c853cc3b
wuxiaogang <940374606@qq.com>
2018-07-27 08:48:54 +0800

    样式

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
 /**
- * @描述:
+ * @描述: 博客首页逻辑代码
  * @作者: wuxiaogang
  * @日期: 2018/7/26
- * @对象名:
  */
+
+var IndexPage = {
+	init : function(){
+		IndexPage.blogDate = $('#blogDate');
+		IndexPage.date.init();
+	},
+	date : {
+		init : function(){
+			IndexPage.blogDate.html(this.getDateData());
+			this.runTime();
+		},
+		getDateData : function(){
+			var nowDate = new Date();
+			var year = nowDate.getFullYear();
+			var month = nowDate.getMonth() + 1;
+			var date = nowDate.getDate();
+			var hour = nowDate.getHours();
+			var min = nowDate.getMinutes();
+			var sec = nowDate.getSeconds();
+			return year + '-' + this.addZero(month) + '-' + this.addZero(date) + ' ' +
+				this.addZero(hour) + ':' + this.addZero(min) + ':' + this.addZero(sec);
+		},
+		runTime : function(){
+			setInterval(function(){
+				IndexPage.blogDate.html(IndexPage.date.getDateData());
+			}, 1000);
+		},
+		addZero : function(num){
+			return num < 10 ? '0' + num : num;
+		}
+	}
+};
+IndexPage.init();
