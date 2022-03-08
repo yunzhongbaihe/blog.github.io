63562b8844b51fb66a3de322feec025f26e93218
吴小刚 <940374606@qq.com>
2022-03-08 10:39:50 +0800

    111

--- js/mobile.js
+++ js/mobile.js
@@ -1,13 +1,13 @@
-~function(){
-	var computed = function(){
-		var winW = document.documentElement.clientWidth;
-		if(winW > 640 && parseInt(document.documentElement.style.fontSize) >= 0){
-		    window.location.reload(true);
-		    return false;
-		}
-		var value = winW < 640 ? winW / 640 * 100 : null;
-		document.documentElement.style.fontSize = value + 'px';
-	};
-	computed();
-	window.addEventListener('resize', computed);
+~function () {
+  var computed = function () {
+    var winW = document.documentElement.clientWidth;
+    if (winW > 640 && parseInt(document.documentElement.style.fontSize) >= 0) {
+      window.location.reload(true);
+      return false;
+    }
+    var value = winW < 640 ? winW / 640 * 100 : null;
+    document.documentElement.style.fontSize = value + "px";
+  };
+  computed();
+  window.addEventListener("resize", computed);
 }();
